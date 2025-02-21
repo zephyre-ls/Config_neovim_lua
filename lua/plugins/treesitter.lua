b25c0ff5076fde2114c7702147bfd8a9b6fe4961
LaurelineD <laurelineduf@proton.me>
2025-02-21 19:45:20 +0100

    Update treesitter.lua

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,37 +1,41 @@
+-- pour l'instant qd je fais ctrl + espace ca me deplace juste de mot en mot
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
+  "nvim-treesitter/nvim-treesitter",  
+  build = ":TSUpdate",  -- qd plugin installé, la commande ":TSUpdate" est exe pour s'assurer que les parsers sont à jour
   config = function()
-    local treesitter = require("nvim-treesitter.configs")
+    local treesitter = require("nvim-treesitter.configs") 
 
     -- configuration de treesitter
-    treesitter.setup({
+    treesitter.setup({ 
       -- activation de la coloration syntaxique
-      highlight = {
-        enable = true,
+      highlight = {  
+        enable = true, 
       },
+
       -- activation de l'indentation améliorée
-      indent = { enable = true },
+      indent = { 
+        enable = true, 
+      },
 
       -- langages installés et configurés
-      ensure_installed = {
+      ensure_installed = {  -- listes des langages installés automatiquement
         "bash",
-				"c",
+        "c",
         "lua",
-        "vim",
+        "vim",  -- (configurations et scripts Vim)
       },
-      -- lorse de l'appui sur <Ctrl-space> sélectionne le bloc
-      -- courant spécifique au langage de programmation
-      incremental_selection = {
+
+      --  <Ctrl-space> sélectionne le bloc courant spécifique au langage de programmation
+      incremental_selection = {  --sélectionner progressivement des blocs de code
         enable = true,
-        keymaps = {
-          init_selection = "<C-space>",
-          node_incremental = "<C-space>",
+        keymaps = {  -- config des raccourcis clavier
+          init_selection = "<C-space>", 
+          node_incremental = "<C-space>", 
           scope_incremental = false,
           node_decremental = "<bs>",
         },
       },
     })
-  end,
+  end, 
 }
 
