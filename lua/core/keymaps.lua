3b893043e2bf442e72638cdbc55323d1dc1fd0c2
Zephyre <laurelineduf@proton.me>
2025-04-03 11:30:21 +0200

    rajout commentaire

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,31 +1,47 @@
--- Touche leader sur espace
+-- Touche leader = touche qui sert de préfixe pour créer mes raccourcis clavier.
+-- Par défaut (\);
+-- Touche choisi = espace;
 vim.g.mapleader = " "
 
--- Raccourci pour la fonction set
+-- Raccourci pour la fonction set (pour simplifier l'écriture des raccourci clavier)
 local keymap = vim.keymap.set
 
--- on utilise ;; pour sortir du monde insertion
+-- on utilise ;; pour sortir du monde insertion / ou ECHAP
 keymap("i", ";;", "<ESC>", { desc = "Sortir du mode insertion avec ;;" })
 
 -- on efface le surlignage de la recherche
+-- en mode normal;
+-- espace + n + h = désactive surlignage des recherches
 keymap("n", "<leader>nh", ":nohl<CR>", { desc = "Effacer le surlignage de la recherche" })
 
--- I déplace le texte sélectionné vers le haut en mode visuel (activé avec v)
+-- en mode visuel v;
+-- selection du texte avec v;
+-- maj + I = monte la selection d'une ligne
+-- maj + K = descend la selection d'une ligne
 keymap("v", "<S-i>", ":m .-2<CR>==", { desc = "Déplace le texte sélectionné vers le haut en mode visuel" })
 -- K déplace le texte sélectionné vers le bas en mode visuel (activé avec v)
 keymap("v", "<S-k>", ":m .+1<CR>==", { desc = "Déplace le texte sélectionné vers le bas en mode visuel" })
 
--- I déplace le texte sélectionné vers le haut en mode visuel bloc (activé avec V)
+-- en mode visuel ligne V (x);
+-- maj + I = monte;
+-- maj + K = descend;
 keymap("x", "<S-i>", ":move '<-2<CR>gv-gv", { desc = "Déplace le texte sélectionné vers le haut en mode visuel bloc" })
--- K déplace le texte sélectionné vers le bas en mode visuel (activé avec V)
 keymap("x", "<S-k>", ":move '>+1<CR>gv-gv", { desc = "Déplace le texte sélectionné vers le bas en mode visuel bloc" })
 
--- Changement de fenêtre avec Ctrl + déplacement uniquement au lieu de Ctrl-w + déplacement
+-- Deplacement rapide entre les fenêtres lorsqu'on passe par : :split/:vsplit;
+-- mode = n 
+-- ctrl + H gauche;
+-- ctrl + J bas;
+-- ctrl + K haut;
+-- ctrl + L droite;
 keymap("n", "<C-h>", "<C-w>h", { desc = "Déplace le curseur dans la fenêtre de gauche" })
 keymap("n", "<C-j>", "<C-w>j", { desc = "Déplace le curseur dans la fenêtre du bas" })
 keymap("n", "<C-k>", "<C-w>k", { desc = "Déplace le curseur dans la fenêtre du haut" })
 keymap("n", "<C-l>", "<C-w>l", { desc = "Déplace le curseur dans la fenêtre droite" })
 
--- Navigation entre les buffers
+-- mode : n
+-- ouverture de plusieurs fichier :e fichier.c
+-- maj + L = buff suivant;
+-- maj + H = buff precedent;
 keymap("n", "<S-l>", ":bnext<CR>", opts)
 keymap("n", "<S-h>", ":bprevious<CR>", opts)
